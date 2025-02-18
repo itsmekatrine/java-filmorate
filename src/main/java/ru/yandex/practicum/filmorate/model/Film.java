f44cf5c95132487083abec602ea6be9967a62f1e
itsmekatrine <itsmekatrine1@gmail.com>
2025-02-18 12:26:07 +0300

    Добавление валидации минимальной даты для releaseDate

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -15,9 +15,14 @@ public class Film {
     @Size(max = 200, message = "Описание не может превышать 200 символов")
     private String description;
 
-    @NotNull
-    @PastOrPresent(message = "Дата релиза не может быть раньше 28 декабря 1895 года")
+    @NotNull(message = "Дата релиза не может быть пустой")
     private LocalDate releaseDate;
+
+    @AssertTrue(message = "Дата релиза не может быть раньше 28 декабря 1895 года")
+    public boolean isValidReleaseDate() {
+        LocalDate earliestDate = LocalDate.of(1895, 12, 28);
+        return releaseDate == null || !releaseDate.isBefore(earliestDate);
+    }
 
     @Positive(message = "Продолжительность фильма должна быть положительной")
     private int duration;
