1ef316a5ccc7d34b3040d97686b426b761836c10
itsmekatrine <itsmekatrine1@gmail.com>
2025-04-01 17:52:36 +0300

    Обновление модели данных в коде

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -24,4 +24,8 @@ public class Film {
     private Integer duration;
 
     private Set<Integer> likes = new HashSet<>();
+
+    private MPARating mpa;
+
+    private Set<Genre> genres = new HashSet<>();
 }
