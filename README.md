9f4bd024b9b4e512ed57e5d826f07197ae080bb7
itsmekatrine <itsmekatrine1@gmail.com>
2025-04-01 17:41:39 +0300

    - Добавление зависимостей H2 - Формирование структуры базы данных

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Template repository for Filmorate project.
 
 Ниже представлена схема базы данных:
 
-![Image database](https://github.com/itsmekatrine/java-filmorate/blob/add-friends-likes/src/main/java/ru/yandex/practicum/filmorate/database/database_schema.jpeg#:~:text=database_schema.jpeg)
+![Image database](https://github.com/itsmekatrine/java-filmorate/blob/add-friends-likes/src/main/java/ru/yandex/practicum/filmorate/resources/database_schema.jpeg#:~:text=database_schema.jpeg)
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,50 @@
+-- Таблица рейтинга
+CREATE TABLE IF NOT EXISTS mpa_ratings (
+    rating VARCHAR(10) PRIMARY KEY,
+    description VARCHAR(200)
+);
+
+-- Таблица фильмов
+CREATE TABLE IF NOT EXISTS films (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(200),
+    release_date DATE,
+    duration INT,
+    mpa_rating VARCHAR(10),
+    FOREIGN KEY (mpa_rating) REFERENCES mpa_ratings(rating)
+);
+
+-- Таблица жанров
+CREATE TABLE IF NOT EXISTS genres (
+    id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+-- Таблица фильмов и жанров
+CREATE TABLE IF NOT EXISTS film_genres (
+    film_id INT,
+    genre_id INT,
+    PRIMARY KEY (film_id, genre_id),
+    FOREIGN KEY (film_id) REFERENCES films(id),
+    FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
+
+-- Таблица пользователей
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(200) NOT NULL,
+    login VARCHAR(100) NOT NULL,
+    name VARCHAR(200),
+    birthday DATE
+);
+
+-- Таблица друзей пользователя
+CREATE TABLE IF NOT EXISTS friendships (
+    user_id INT,
+    friend_id INT,
+    status VARCHAR(20),
+    PRIMARY KEY (user_id, friend_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (friend_id) REFERENCES users(id)
+);
