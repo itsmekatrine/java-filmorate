9c38a91f5b2e89b35fa2845b2189b430cc3179ea
itsmekatrine <itsmekatrine1@gmail.com>
2025-04-02 13:41:16 +0300

    Исправления для Checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmDbStorageTest.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
